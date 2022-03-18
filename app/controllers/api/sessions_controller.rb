4c1697e493bef2236156663dc392d396e2d39c9c
johnnyhoang510 <johnnyhoang510@yahoo.com>
2022-03-18 12:12:11 -0700

    ajax requests working

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,21 @@
+class Api::SessionsController < ApplicationController
+
+    def create
+        @user = User.find_by_credentials(
+            params[:user][:email], 
+            params[:user][:password])
+
+        if @user
+            login!(@user)
+            render "api/users/show"
+        else
+            render json: ['Invalid email/password'], status: 401
+        end
+    end
+
+    def destroy
+        logout!
+        render html: "static_pages/root"
+    end
+
+end
