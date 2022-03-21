9c38f595261c016c6884b39d9c8c0631a97f4635
johnnyhoang510 <johnnyhoang510@yahoo.com>
2022-03-21 13:40:20 -0700

    prepare for heroku push

--- frontend/components/business/business_show_page.jsx
+++ frontend/components/business/business_show_page.jsx
@@ -4,22 +4,22 @@ import { withRouter } from "react-router";
 class BusinessShowPage extends React.Component {
     constructor(props) {
         super(props);
-        console.log(this.props)
-        console.log("from constructor")
+        // console.log(this.props)
+        // console.log("from constructor")
     }
     
     componentDidMount() {
         this.props.fetchBusiness(this.props.match.params.businessId);
-        console.log("from componentdidmount")
-        console.log(this.props);
+        // console.log("from componentdidmount")
+        // console.log(this.props);
     };
     
 
     render() {
 
         const { business } = this.props;
-        console.log(this.props);
-        console.log("from render")
+        // console.log(this.props);
+        // console.log("from render")
         return(
             <div>
                 <h1>Welcome to the show page</h1>
