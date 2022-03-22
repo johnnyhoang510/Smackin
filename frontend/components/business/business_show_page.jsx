e2048c5d5969628505791b4562bafc972f7b2b52
johnnyhoang510 <johnnyhoang510@yahoo.com>
2022-03-21 17:15:05 -0700

    biz show details

--- frontend/components/business/business_show_page.jsx
+++ frontend/components/business/business_show_page.jsx
@@ -1,9 +1,15 @@
 import React from "react";
-import { withRouter } from "react-router";
+import NavBarContainer from "../navbar/navbar_container";
 
 class BusinessShowPage extends React.Component {
     constructor(props) {
         super(props);
+
+        //extra feature, add a open now or closed marker depending on current time/day
+        this.time = new Date();
+        this.minutes = this.time.getMinutes();
+        this.hours = this.time.getHours() - 12;
+        this.day = this.time.getDay();
     }
     
     componentDidMount() {
@@ -14,7 +20,7 @@ class BusinessShowPage extends React.Component {
     
     componentDidUpdate(prevProps) {
         if (prevProps.businessId !== this.props.businessId) {
-            this.props.fetchBusiness(this.props.businessId)
+            this.componentDidMount();
         }
     }
     
@@ -23,13 +29,90 @@ class BusinessShowPage extends React.Component {
         // console.log(this.props)
         const { business } = this.props;
 
-        // on a re render, this will allow it to hit compDidMount
-        if (business === undefined) return null;
+        // on a rerender, this will allow it to hit compDidMount
+        if (!business) return null;
         
         return(
-            <div>
-                {business.name}
-                <h1>Welcome to the show page</h1>
+            <div className="biz-show-container">
+
+                <div className="biz-show-nav">
+                    <NavBarContainer />
+                </div>
+
+                <div className="biz-show-header">
+                    <h1 className="biz-show-name">{business.name}</h1>
+
+                    <div className="biz-show-reviews-section">
+                        <p className="biz-show-avgRating">Avg Rating here</p>
+                        <p className="biz-show-numReviews"># of Reviews here</p>
+                    </div>
+
+                    <div className="biz-show-details-info">
+                        <p className="biz-show-details-claimed">Claimed</p>
+                        <p className="biz-show-details-price">{business.price}</p>
+                        <p className="biz-show-details-category">{business.category}</p>
+                        <p className="biz-show-details-hours">{business.hours}</p>
+                    </div>
+                </div>
+
+                <div className="biz-show-write-review-container">
+                    <p className="biz-show-write-review-link">Write a Review (link to review)</p>
+                </div>
+
+                <div className="biz-show-location-hours-container">
+                    <h1 className="biz-show-location-hours-title">Location &amp; Hours</h1>
+
+                    <div className="biz-show-location-hours-map-container">
+                        <p>MAP COMPONENT/LINK GOES HERE</p>
+                        <p className="biz-show-address">{business.address}</p>
+                        <p className="biz-show-city-state">{business.city}, {business.state}</p>
+                        <p className="biz-show-zip-code">{business.zip_code}</p>
+                    </div>
+
+                    <div className="biz-show-hours-container">
+                        <div className="biz-show-hours-item">
+                            <p className="biz-show-hours-item-day">Mon</p>
+                            <p className="biz-show-hours-item-hours">{business.hours}</p>
+                        </div>
+                        <div className="biz-show-hours-item">
+                            <p className="biz-show-hours-item-day">Tue</p>
+                            <p className="biz-show-hours-item-hours">{business.hours}</p>
+                        </div>
+                        <div className="biz-show-hours-item">
+                            <p className="biz-show-hours-item-day">Wed</p>
+                            <p className="biz-show-hours-item-hours">{business.hours}</p>
+                        </div>
+                        <div className="biz-show-hours-item">
+                            <p className="biz-show-hours-item-day">Thu</p>
+                            <p className="biz-show-hours-item-hours">{business.hours}</p>
+                        </div>
+                        <div className="biz-show-hours-item">
+                            <p className="biz-show-hours-item-day">Fri</p>
+                            <p className="biz-show-hours-item-hours">{business.hours}</p>
+                        </div>
+                        <div className="biz-show-hours-item">
+                            <p className="biz-show-hours-item-day">Sat</p>
+                            <p className="biz-show-hours-item-hours">{business.hours}</p>
+                        </div>
+                        <div className="biz-show-hours-item">
+                            <p className="biz-show-hours-item-day">Sun</p>
+                            <p className="biz-show-hours-item-hours">{business.hours}</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="biz-show-aside-info">
+                    <div className="biz-show-aside-info-website">
+                        <a className="biz-show-website-link" href={`${business.website}`} target="_blank">{business.website}</a>
+                    </div>
+                    <div className="biz-show-aside-info-phone-number">
+                        <p className="biz-show-phone-number">{business.phone_number}</p>
+                    </div>
+                    <div className="biz-show-aside-info-directions">
+                        <a href="#" target="_blank">Get Directions (need to source link)</a>
+                    </div>
+                </div>
+
             </div>
         )
     }
