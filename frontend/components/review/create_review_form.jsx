7ffa0fc532683368e2a1ef0ab31c7e98e14f4455
johnnyhoang510 <johnnyhoang510@yahoo.com>
2022-03-23 15:40:00 -0700

    started create review form

--- frontend/components/review/create_review_form.jsx
+++ frontend/components/review/create_review_form.jsx
@@ -11,10 +11,21 @@ class CreateReviewForm extends React.Component{
             // business_id is a string? is that normal
             business_id: this.props.match.params.businessId
         }
+
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     componentDidMount() {
         this.props.fetchBusiness(this.props.match.params.businessId)
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        this.props.createReview(this.state, this.props.match.params.businessId)
+    }
+
+    update(field) {
+        return e => this.setState({ [field]: e.currentTarget.value })
     }
 
     render() {
@@ -28,9 +39,29 @@ class CreateReviewForm extends React.Component{
             <div className="create-review-form-container">
                 <h1 className="create-review-form-title">{business.name}</h1>
 
-                <form action="">
+                <form className="create-review-form" onSubmit={this.handleSubmit}>
+                    <div className="create-review-form-rating-wrapper">
+                        <p className="select-your-rating">Select your rating</p>
 
-                    
+                        <label htmlFor="rating-1" className="create-review-form-rating-1">1</label>
+                        <input id="rating-1" type="radio"  value="1" onChange={this.update('rating')} name="rating"/>
+
+                        <label htmlFor="rating-2" className="create-review-form-rating-2">2</label>
+                        <input id="rating-2" type="radio"  value="2" onChange={this.update('rating')} name="rating"/>
+
+                        <label htmlFor="rating-3" className="create-review-form-rating-3">3</label>
+                        <input id="rating-3" type="radio"  value="3" onChange={this.update('rating')} name="rating"/>
+
+                        <label htmlFor="rating-4" className="create-review-form-rating-4">4</label>
+                        <input id="rating-4" type="radio"  value="4" onChange={this.update('rating')} name="rating"/>
+
+                        <label htmlFor="rating-5" className="create-review-form-rating-5">5</label>
+                        <input id="rating-5" type="radio"  value="5" onChange={this.update('rating')} name="rating"/>
+                    </div>
+
+                    <textarea rows="25" cols="70" className="create-review-form-textarea" placeholder="it was trash"></textarea>
+                    <br />
+                    <button type="submit" className="create-review-form-submit">Post Review</button>
                 </form>
             </div>
         )
