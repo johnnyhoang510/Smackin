2e724a0d2d31fb79abebdaa332a6fa0391ba8ab7
johnnyhoang510 <johnnyhoang510@yahoo.com>
2022-11-30 15:23:44 -0800

    refactor session forms

--- frontend/components/session/signup_form.jsx
+++ frontend/components/session/signup_form.jsx
@@ -98,68 +98,71 @@ const SignupForm = (props) => {
                         {displayErrors()}
                 </ul>
 
-                <form className="form-signup" onKeyDown={(e) => submitHandler(e)}>
-                    <h2 className="form-type-title-signup">Sign Up for Smackin'</h2>
+                <div class="signup-form-image-wrapper">
+                    <form className="form-signup" onKeyDown={(e) => submitHandler(e)}>
+                        <h2 className="form-type-title-signup">Sign Up for Smackin'</h2>
 
-                    <div className="switch-form-header-signup">
-                        <h3 className="signup-subtitle">Connect with great local businesses</h3>
-                        <p className="terms-signup">By continuing, you agree to Smackin's Terms of Service and acknowledge Smackin's Privacy Policy.</p>
-                    </div>
+                        <div className="switch-form-header-signup">
+                            <h3 className="signup-subtitle">Connect with great local businesses</h3>
+                            <p className="terms-signup">By continuing, you agree to Smackin's Terms of Service and acknowledge Smackin's Privacy Policy.</p>
+                        </div>
 
-                    <div className="demo-user-signup">
-                        <button className="demo-button-signup" onKeyDown={(e) => submitHandler(e)} onClick={(e) => handleDemoLogin(e)}>Continue as Demo User</button>
-                    </div>
-                    <br />
+                        <div className="demo-user-signup">
+                            <button className="demo-button-signup" onKeyDown={(e) => submitHandler(e)} onClick={(e) => handleDemoLogin(e)}>Continue as Demo User</button>
+                        </div>
+                        <br />
 
-                    <div className="signup-form-divider">OR</div>
+                        <div className="signup-form-divider">OR</div>
 
-                    <br />
-                    <input className="first-name-input"
-                        type="text"
-                        value={firstName}
-                        onChange={(e) => setFirstName(e.currentTarget.value)}
-                        placeholder="First Name"
-                    />
-                    <input className="last-name-input"
-                        type="text"
-                        value={lastName}
-                        onChange={(e) => setLastName(e.currentTarget.value)}
-                        placeholder="Last Name"
-                    />
+                        <br />
+                        <input className="first-name-input"
+                            type="text"
+                            value={firstName}
+                            onChange={(e) => setFirstName(e.currentTarget.value)}
+                            placeholder="First Name"
+                        />
+                        <input className="last-name-input"
+                            type="text"
+                            value={lastName}
+                            onChange={(e) => setLastName(e.currentTarget.value)}
+                            placeholder="Last Name"
+                        />
 
-                    <br />
-                    <input className="signup-input"
-                        type="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.currentTarget.value)}
-                        placeholder="Email"
-                    />
-                    <br />
+                        <br />
+                        <input className="signup-input"
+                            type="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.currentTarget.value)}
+                            placeholder="Email"
+                        />
+                        <br />
 
-                    <input className="signup-input"
-                        type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.currentTarget.value)}
-                        placeholder="Password"
-                    />
-                    <br />
+                        <input className="signup-input"
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.currentTarget.value)}
+                            placeholder="Password"
+                        />
+                        <br />
 
-                    <input className="signup-input"
-                        type="text"
-                        value={zipCode}
-                        onChange={(e) => setZipCode(e.currentTarget.value)}
-                        placeholder="Zip Code"
-                    />
-                    <br />
-                    <button className="signup-submit" type="submit" onClick={(e) => handleSubmit(e)}>Sign Up</button>
-                    <h4 className="switch-form-text-signup">Already on Smackin'?
-                        <Link className="switch-form-signup" to="/login" onClick={() => clearErrors()}>Log in</Link>
-                    </h4>
+                        <input className="signup-input"
+                            type="text"
+                            value={zipCode}
+                            onChange={(e) => setZipCode(e.currentTarget.value)}
+                            placeholder="Zip Code"
+                        />
+                        <br />
+                        <button className="signup-submit" type="submit" onClick={(e) => handleSubmit(e)}>Sign Up</button>
+                        <h4 className="switch-form-text-signup">Already on Smackin'?
+                            <Link className="switch-form-signup" to="/login" onClick={() => clearErrors()}>Log in</Link>
+                        </h4>
 
-                </form>
+                    </form>
+
+                    <img className="signup-form-pic" src={window.signupPic} alt="" />
+                </div>
             </div>
         </div>
-
     )
 };
 
